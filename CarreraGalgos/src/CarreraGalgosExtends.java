fe59d105578795320a96c5fd2923b2898ea1a622
ArandaLidia <arandagomezlidia@gmail.com>
2024-11-13 02:08:09 +0100

    up

---
+++ CarreraGalgos/src/CarreraGalgosExtends.java
@@ -0,0 +1,52 @@
+	import java.util.ArrayList;
+	import java.util.List;
+import java.util.Random;
+import java.util.Scanner;
+
+public class CarreraGalgosExtends extends Thread {
+
+
+	    
+	    private String nombre;
+	    private int tiempoCarrera;
+	    
+	    public CarreraGalgosExtends(String nombre, int tiempoCarrera) {
+	        this.nombre = nombre;
+	        this.tiempoCarrera = tiempoCarrera;
+	    }
+	    
+	    @Override
+	    public void run() {
+	        try {
+	            System.out.printf("El galgo %s empieza a correr.%n", nombre);
+	            Thread.sleep(tiempoCarrera * 1000);
+	            System.out.printf("El galgo %s ha terminado la carrera.%n", nombre);
+	        } catch (InterruptedException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+	        System.out.println("¿Cuántos galgos corren en la carrera?");
+	        int numGalgos = scanner.nextInt();
+	        scanner.nextLine();
+
+	        ArrayList<CarreraGalgosExtends> listaGalgos = new ArrayList<>();
+
+	        for (int i = 0; i < numGalgos; i++) {
+	            System.out.println("Inserta el nombre del galgo " + (i + 1) + ":");
+	            String nombre = scanner.nextLine();
+	            int tiempo = (int)(Math.random()*10);
+
+	            CarreraGalgosExtends carreraGalgos = new CarreraGalgosExtends(nombre, tiempo);
+	            listaGalgos.add(carreraGalgos);
+	        }
+
+	        for (CarreraGalgosExtends galgo : listaGalgos) {
+	            galgo.start();
+	        }
+	    }
+	}
+
+
